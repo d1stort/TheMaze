5136ec8219830b2e18092430f91bed6c9da59551
d1stort <yano4ca@gmail.com>
2016-03-10 19:05:21 +0300

    WPF finally initialized

---
+++ TheMaze/Level1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TheMaze
+{
+    /// <summary>
+    /// Interaction logic for Level1.xaml
+    /// </summary>
+    public partial class Level1 : Window
+    {
+        public Level1()
+        {
+            InitializeComponent();
+        }
+
+        private void MoveMouseToStart()
+        {
+            Point startPoint = new Point();
+            startPoint.Offset(5, 5);
+            //Cursor = PointToScreen(startPoint);
+        }
+
+        private void mouseOver_finish(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Congrads!");
+            Close();
+        }
+    }
+}
