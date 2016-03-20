100e955261c01f78386ca1bee21c801b55a34985
badaevael <badaevael@icloud.com>
2016-03-20 19:03:36 +0300

    Some logic was added

--- TheMaze/Commands/PlayerUpdateCommand.cs
+++ TheMaze/Commands/PlayerUpdateCommand.cs
@@ -30,7 +30,7 @@ namespace TheMaze.Commands
 
         public void Execute(object parameter)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
     }
 }
--- TheMaze/Migrations/Configuration.cs
+++ TheMaze/Migrations/Configuration.cs
@@ -14,6 +14,14 @@ namespace TheMaze.Migrations
 
         protected override void Seed(TheMaze.Context context)
         {
+            Player p1 = new Player { Id = 0, NickName = "badaevael", Score = 10 };
+            Player p2 = new Player { Id = 1, NickName = "d1stort", Score = 10 };
+
+
+                context.Players.AddOrUpdate(p1);
+                context.Players.AddOrUpdate(p2);
+            
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
